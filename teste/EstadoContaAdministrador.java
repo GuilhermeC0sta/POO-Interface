a7ab89c0e319491e74f3604e3277b6efdeec5fbe
Micael <98262812+MicaelSv@users.noreply.github.com>
2023-04-28 19:43:24 -0300

    Add files via upload

---
+++ teste/EstadoContaAdministrador.java
@@ -0,0 +1,12 @@
+//import LoginRegister.TelaInicial;
+public class EstadoContaAdministrador extends EstadoConta {
+
+    public EstadoContaAdministrador(LoginRegister.TelaInicial telaInicial) {
+        super(telaInicial);
+    }
+
+    @Override
+    public void executarAcoes() {
+        // Executar ações para conta administrador
+    }
+}
