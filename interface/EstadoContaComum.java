72f10d81a5493e36e5042cf64890c1b2aeacbf76
Micael <98262812+MicaelSv@users.noreply.github.com>
2023-04-29 11:00:30 -0300

    Add files via upload

---
+++ interface/EstadoContaComum.java
@@ -0,0 +1,12 @@
+//import LoginRegister.TelaInicial;
+public class EstadoContaComum extends EstadoConta {
+    public EstadoContaComum(LoginRegister.TelaInicial telaInicial) {
+        super(telaInicial);
+    }
+
+    @Override
+    public void executarAcoes() {
+        // Executar ações para conta comum/premium
+    }
+}
+
