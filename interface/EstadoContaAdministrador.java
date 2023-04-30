d3241d9adbb32e3c394b8f5dbfbb83cd78c9461e
Micael <98262812+MicaelSv@users.noreply.github.com>
2023-04-30 09:06:57 -0300

    Update EstadoContaAdministrador.java

--- interface/EstadoContaAdministrador.java
+++ interface/EstadoContaAdministrador.java
@@ -1,12 +1,31 @@
 //import LoginRegister.TelaInicial;
-public class EstadoContaAdministrador extends EstadoConta {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.List;
+import java.util.regex.*;
+import java.util.ArrayList;
 
-    public EstadoContaAdministrador(LoginRegister.TelaInicial telaInicial) {
+
+public class EstadoContaAdministrador extends EstadoConta{
+    
+    public ArrayList<conta> contas;
+    LoginRegister dados;
+
+    public EstadoContaAdministrador(LoginRegister.TelaInicial telaInicial, ArrayList<conta> contas, LoginRegister dados){
         super(telaInicial);
+        this.dados = dados;
+        this.contas = contas;
     }
 
     @Override
-    public void executarAcoes() {
-        // Executar ações para conta administrador
+    public void mostrarTela() {
+        //LoginRegister teste = new LoginRegister();
+        //teste.getContas();
+
+        telaAdmin novatela = new telaAdmin(contas, dados);
+        novatela.setVisible(true);
     }
+
 }
+
