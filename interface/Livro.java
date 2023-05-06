6dca3ac3ada9b41e9ee3d1b6aa6043b8fcba8f49
Jani0Marinho <125390191+Jani0Marinho@users.noreply.github.com>
2023-05-06 09:10:54 -0300

    Update Livro.java

--- interface/Livro.java
+++ interface/Livro.java
@@ -4,16 +4,20 @@ public class Livro extends ItemsBiblioteca {
     private int isbn;
     private String genero;
     private String titulo;
-    private int qntd_disp;
+
 
     public Livro(String titulo, String autor, int isbn, int qntd_disp, String genero) {
         super(titulo, qntd_disp);
-        this.qntd_disp = qntd_disp;
         this.titulo = titulo;
         this.isbn = isbn;
         this.autor = autor;
         this.genero = genero;
-        state = new AvailableState();
+        if(qntd_disp > 0){
+            state = new AvailableState();
+        }
+        else{
+            state = new OutOfStockState();
+        }
     }
 
     @Override
@@ -43,7 +47,6 @@ public class Livro extends ItemsBiblioteca {
 
     @Override
     public void locar() {
-
         System.out.println("\nVocê conseguiu locar um livro! Devolva dentro de 10 dias.\n");
         System.out.println(" Este livro possui as seguintes especificações: ");
         System.out.println(" Título: " + getTitulo());
@@ -62,9 +65,8 @@ public class Livro extends ItemsBiblioteca {
     }
 
     public void rent() {
-        // Verifica se o livro está disponível para aluguel.
         if (state.isAvailable()) {
-            qntd_disp--;
+            this.qntd_disp--;
             updateState();
         } else {
             throw new IllegalStateException("Livro não disponível para aluguel.");
